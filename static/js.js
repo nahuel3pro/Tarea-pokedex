7031395f26392125941d9614bb2cd2c0cf18f99b
nahuel3pro <82716025+nahuel3pro@users.noreply.github.com>
2021-12-08 17:58:00 -0300

    Terminada navegación con las flechitas
    Se agregaron estilos a la página, además de un logo. Y se terminó la funcionalidad de los botones para avanzar o retroceder de página.

--- static/js.js
+++ static/js.js
@@ -1,7 +1,14 @@
 const $grid = document.querySelector('#mygrid');
 const $pokepelota = document.querySelector('#pokepelota');
+const $siguiente = document.querySelector('#siguiente');
+const $anterior = document.querySelector('#anterior');
+const $first = document.querySelector('#first');
+const $logo = document.querySelector('#logo');
+const $barra = document.querySelector('#barrabusqueda');
+
 const CANTIDAD_DE_CARTAS = 12;
-let numeroDePagina = 0;
+let pagina = 0;
+let numeroDePagina = 1;
 
 const URL = 'https://pokeapi.co/api/v2/';
 let pokemones = []
@@ -11,16 +18,80 @@ function esconderAnimacionPokepelota() {
     $pokepelota.classList.add('d-none');
 }
 
-function mostrarCartas() {
-    $grid.classList.remove('d-none');
-    document.querySelector('#barrabusqueda').classList.remove('d-none');
-    document.querySelector('body').style.removeProperty('display');
-    document.querySelector('body').style.removeProperty('height');
+function esconderLogo() {
+    $logo.classList.add('d-none');
 }
 
-function animacionPokepelota() {
+function mostrarLogo() {
+    $logo.classList.remove('d-none');
+}
+
+function esconderBarra() {
+    $barra.classList.add('d-none');
+}
+
+function mostrarBarra() {
+    $barra.classList.remove('d-none');
+}
+
+function esconderCartas() {
+    $grid.classList.add('d-none');
+}
+
+function mostrarCartas() {
+    $grid.classList.remove('d-none');
+}
+
+function esconderSiguiente() {
+    $siguiente.classList.add('d-none');
+}
+
+function esconderAnterior() {
+    $anterior.classList.add('d-none');
+}
+
+function mostrarSiguiente() {
+    $siguiente.classList.remove('d-none');
+}
+
+function mostrarAnterior() {
+    $anterior.classList.remove('d-none');
+}
+
+function mostrarFirst(){
+    $first.classList.remove('d-none');
+}
+
+function esconderFirst(){
+    $first.classList.add('d-none');
+}
+
+function mostrarPokepelota() {
     $pokepelota.classList.remove('d-none');
 }
+
+function mostrarCartasyPagina() {
+    mostrarCartas();
+
+    document.querySelector('body').style.removeProperty('display');
+    document.querySelector('body').style.removeProperty('height');
+
+    mostrarBarra();
+    mostrarLogo();
+}
+
+function manejarFlechas() {
+    if (numeroDePagina === 1) {
+        esconderFirst();
+        mostrarSiguiente();
+        esconderAnterior();
+    } else {
+        mostrarFirst();
+        mostrarSiguiente();
+        mostrarAnterior();
+    }
+}
+
 
 function addPokemonToPage(value, index) {
     document.querySelectorAll('.pokemon-nombre')[index].innerHTML = `
@@ -30,48 +101,75 @@ function addPokemonToPage(value, index) {
     `
 }
 
+function esconderPagina() {
+
+    esconderLogo();
+
+    esconderAnterior();
+    esconderSiguiente();
+
+    esconderBarra();
+
+    esconderCartas();
+
+}
+
 function fetchpokemones() {
-    fetch(URL + `pokemon?limit=${CANTIDAD_DE_CARTAS}&offset=${numeroDePagina}`)
-        .then(respuesta => respuesta.json())
-        .then(respuestaJSON => {
-            resultados = respuestaJSON.results;
+    fetch(URL + `pokemon?limit=${CANTIDAD_DE_CARTAS}&offset=${pagina}`)
+        .then(pokemonAPIresponse => pokemonAPIresponse.json())
+        .then(pokemonAPIresponseJSON => {
+            resultados = pokemonAPIresponseJSON.results;
 
             Object.keys(resultados).forEach((value, index) => {
                 addPokemonToPage(value, index);
             })
 
             esconderAnimacionPokepelota();
-            mostrarCartas();
+            mostrarCartasyPagina();
+
+            manejarFlechas();
+
         })
         .catch(() => alert('Hubo un error, intente nuevamente o más tarde.'));
 
-    animacionPokepelota();
+    mostrarPokepelota();
 }
 
-// function randomArray(array) {
-//     let m = array.length, t, i;
-
-//     while (m) {
-
-//         i = Math.floor(Math.random() * m--);
-
-//         t = array[m];
-//         array[m] = array[i];
-//         array[i] = t;
-//     }
-
-//     return array;
-// }
+fetchpokemones();
 
 for (i = 0; i < CANTIDAD_DE_CARTAS; i++) {
     $grid.innerHTML += (`
     
     <div class="card bg border border-5">
-        <img src="static/pokepelota.png" alt="pokeball">
-        <h2 class="card p-1 text-center pokemon-nombre"></h2>
-        </div>
+    <img src="static/img/pokepelota.png" alt="pokeball">
+    <h2 class="card p-1 text-center pokemon-nombre justify-content-center"></h2>
+    </div>
     
     `)
 }
 
-fetchpokemones();
+$siguiente.onclick = function (e) {
+    console.log('Siguiente');
+
+    numeroDePagina++;
+    pagina += CANTIDAD_DE_CARTAS
+
+    esconderPagina();
+
+    fetchpokemones();
+
+    e.preventDefault();
+}
+
+$anterior.onclick = function (e) {
+    console.log('Siguiente');
+
+    numeroDePagina--;
+    pagina -= CANTIDAD_DE_CARTAS
+
+    esconderPagina();
+
+    fetchpokemones();
+
+    e.preventDefault();
+}
